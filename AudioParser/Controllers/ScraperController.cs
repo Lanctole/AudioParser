1711a29e9547e928023e9b17f247835d12010ddb
lanct <lanct@LANCTOLE>
2023-06-14 12:23:40 +0300

    Добавьте файлы проекта.

---
+++ AudioParser/Controllers/ScraperController.cs
@@ -0,0 +1,29 @@
+﻿using AudioParser.Models;
+using HtmlAgilityPack;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AudioParser.Controllers
+{
+    public class ScraperController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Parse(string url,string saveDir)
+        {
+            AudioSaver saver = new AudioSaver(url,saveDir);
+            //ViewBag.Message = "Done";
+            //var title = doc.DocumentNode.SelectSingleNode("//title").InnerText;
+            return Json( "Done");
+            //var web = new HtmlWeb();
+            //var doc = web.Load(url);
+            //var title = doc.DocumentNode.SelectSingleNode("//title").InnerText;
+            //var description = doc.DocumentNode.SelectSingleNode("//meta[@name='description']")?.Attributes["content"]?.Value;
+            //var keywords = doc.DocumentNode.SelectSingleNode("//meta[@name='keywords']")?.Attributes["content"]?.Value;
+            //return Json(new { title, description, keywords });
+        }
+    }
+}
---
+++ AudioParser/Models/AudioSaver.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Extensions.Options;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Net;
+using System.Security.Policy;
+
+namespace AudioParser.Models
+{
+    public class AudioSaver
+    {
+        public AudioSaver(string url,string saveDir) {
+            
+            string audioSelector = "audio";
+
+            ChromeOptions options = new ChromeOptions();
+            options.AddArgument("--headless");
+            using (IWebDriver driver = new ChromeDriver(options))
+            {
+                Directory.CreateDirectory(saveDir);
+                driver.Navigate().GoToUrl(url);
+                IReadOnlyCollection<IWebElement> audioElements = driver.FindElements(By.CssSelector(audioSelector));
+                foreach (IWebElement audioElement in audioElements)
+                {
+                    string audioUrl = audioElement.GetAttribute("src");
+                    WebClient webClient = new WebClient();
+                    int lastSlashIndex = audioUrl.LastIndexOf('/');
+                    string filename = audioUrl.Substring(lastSlashIndex + 1);
+                    if(string.IsNullOrEmpty(audioUrl))
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                       webClient.DownloadFile(audioUrl, saveDir + "/" + filename);
+                    }
+                    
+                }
+            }
+        }
+       
+    }
+} 
+
+
